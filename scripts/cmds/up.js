97a4c8becbbe65e7657fd7dcd97d0d5166edcefa
XAIKO-JADID420Erro <mstdipanita776@gmail.com>
2024-12-17 15:36:38 +0600

    Update up.js

--- scripts/cmds/up.js
+++ scripts/cmds/up.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "up",
     aliases: ["up", "upt"],
     version: "1.0",
-    author: "𝙓𝘼𝙄𝙆𝙊 𝙅𝙄𝙎𝘼𝙉 𝘽𝙊𝙏 𝘽𝙊𝙓",
+    author: "𝙓𝘼𝙄𝙆𝙊-𝙅𝘼𝘿𝙄𝘿 𝘽𝙊𝙏㊝𝘽𝙊𝙓",
     role: 0,
     shortDescription: {
       en: "Displays the uptime of the bot."
@@ -22,7 +22,7 @@ module.exports = {
     const minutes = Math.floor((uptime / 60) % 60);
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `╭──𝙓𝘼𝙄𝙆𝙊-𝙅𝙄𝙎𝘼𝙉 𝘽𝙊𝙏 𝘽𝙊𝙓✤ 𝚄𝙿 𝚃𝙸𝙼𝙴⏳👈\n├⏳ 𝙷𝙾𝚄𝚁𝚂 ${hours}\n├⏰ 𝙼𝙸𝙽𝚄𝚃𝙴𝚂 ${minutes}\n├⏲ 𝚂𝙴𝙲𝙾𝙽𝙳𝚂 ${seconds}\n╰───────────✰`;
+    const uptimeString = `╭──𝙓𝘼𝙄𝙆𝙊-𝙅𝘼𝘿𝙄𝘿 𝘽𝙊𝙓✤ 𝚄𝙿 𝚃𝙸𝙼𝙴⏳👈\n├⏳ 𝙷𝙾𝚄𝚁𝚂 ${hours}\n├⏰ 𝙼𝙸𝙽𝚄𝚃𝙴𝚂 ${minutes}\n├⏲ 𝚂𝙴𝙲𝙾𝙽𝙳𝚂 ${seconds}\n╰───────────✰`;
     api.sendMessage(`${uptimeString}`, event.threadID);
   }
 }
