592d0ef9b96f9e4b223ac5d60a2a4b8ea3eaf074
XAIKO-JADID420Erro <mstdipanita776@gmail.com>
2024-12-18 15:47:19 +0600

    Create 😌.js

---
+++ scripts/cmds/😌.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "😌",
+	 version: "1.0",
+	 author: "Jadid",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "😌") {
+ return message.reply({
+ body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝙅𝘼𝘿𝙄𝘿」",
+ attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=1AcsXnDzyHl3AsglSw-ReP3PA3hYMOY_h")
+ });
+ }
+ }
+}
