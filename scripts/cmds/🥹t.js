2d2ecc5498fd5dd045c25cf507350e053f02834e
XAIKO-JADID420Erro <mstdipanita776@gmail.com>
2024-12-20 18:26:06 +0600

    Create 🥹t.js

---
+++ scripts/cmds/🥹t.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🥹",
+        version: "1.0",
+        author: "Jadid", //** original author fb I'd : https://C.E.O.NOBITA.2021 **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "🥹") return message.reply("কি হইছে বাবু কান্দ কেন-!!😦");
+}
+};
