7c8820d793f300c65f0facd0acf9faec70de6deb
MR-XAIKO JISAN <mshiur9080@gmail.com>
2025-01-24 21:16:32 +0600

    Update sad.js

--- scripts/cmds/sad.js
+++ scripts/cmds/sad.js
@@ -1,31 +1,70 @@
-const axios = require("axios");
-
 module.exports = {
   config: {
     name: "sad",
-    aliases: ["sad quote"],
     version: "1.0",
-    author: "RICKCIEL X KSHITIZ",
-    countDown: 5,
+    author: "jisan",
+    countDown: 20,
     role: 0,
-    shortDescription: "GENERAL QUOTES",
-    longDescription: {
-      en: "Get random sad quotes.",
-    },
-    category: "fun",
-    guide: {
-      en: "{prefix}sad",
-    },
+    shortDescription: "get sad video",
+    longDescription: "get random sad video",
+    category: "SAD",
+    guide: "{pn} sadvdo",
   },
 
-  onStart: async function ({ api, event }) {
-    try {
-      const response = await axios.get("https://api-1.chatbotmesss.repl.co/api/sadquotes1");
-      const { quote, author } = response.data;
-      const message = ` ${quote}`;
-      return api.sendMessage(message, event.threadID);
-    } catch (error) {
-      console.error(error);
+  sentVideos: [],
+
+  onStart: async function ({ api, event, message }) {
+    const senderID = event.senderID;
+
+    const loadingMessage = await message.reply({
+      body: "Loading random sad video... Please wait! 🕐",
+    });
+
+    const link = [
+      "https://drive.google.com/uc?id=16KeE4J7L2Pd8cCKIBvlwEPP07A92b-eb",// video credits xenoz (youtube) "https://drive.google.com/uc?id=16MhNPi_H0-tEe5PQrrqkx_l7SrC_l0kd",
+    "https://drive.google.com/uc?id=15w4cvYmKrCW2Hul2AcvPEk5S4b-CH3EE",
+    "https://drive.google.com/uc?id=16Xa6thSHdEGCiypaetbAEqVCwEAzFnKX",
+    "https://drive.google.com/uc?id=16BnRPvKQd7gd3YLR_rB9QNZymotMqHu7",
+    "https://drive.google.com/uc?id=15fDe2735O50z-3G4yQ5tDT9J873x5izm",
+    "https://drive.google.com/uc?id=16HgiGU7_Cdh8NtpsKi92dTJmALJCV8jD",
+    "https://drive.google.com/uc?id=16KTSrInqvioGnT7RrAskjHYqz8R6RgNY",
+    "https://drive.google.com/uc?id=162yWrNRRTeN4tFEjQEtsR4p-4gWbTFaS",
+    "https://drive.google.com/uc?id=16-q768c6nXstZEjQhWa1pZUPL2Xpjwo9",
+    "https://drive.google.com/uc?id=15bfkP01mTzXutgP_0Z1iyud7SXqq-jOt",
+    "https://drive.google.com/uc?id=15WnvdFOQIhKQ1nlZgsABXaf6Q2nQexGW",
+    "https://drive.google.com/uc?id=16GTgYVSIDduUs4VTxadIzPPyp9KA_102",
+    "https://drive.google.com/uc?id=15Y2GnA-Kcox8Mw6jioxHc1G1yP4pihnC",
+    "https://drive.google.com/uc?id=16-qsG6oldtJiGq11Q3bFxKzuZJRFnoPT",
+    "https://drive.google.com/uc?id=15W8ETDBXrn_JvealPwPFQ2CjvZp437-g",
+    "https://drive.google.com/uc?id=15StZMKfsTdAhhECdKjS6FUFwG_OIHa7W",
+    "https://drive.google.com/uc?id=16lOXxs-Z9u-mxttFnwWzdUHvrP55aHnZ",
+    "https://drive.google.com/uc?id=162Qn-pcnc9iijg5dv59S9DTTQOofL4Fy",
+    "https://drive.google.com/uc?id=1680rf1wQ2TrRuSLHtTwFC7GYctJAnHaX",
+    "https://drive.google.com/uc?id=16-XtMXpa4r1iFJTBS2N68ARMuDH2IWpG",
+    "https://drive.google.com/uc?id=15bO3lguAxsMZPvKkcvlsM6ObXOfJMz79",
+     // Add more video links here
+    ];
+
+    const availableVideos = link.filter(video => !this.sentVideos.includes(video));
+
+    if (availableVideos.length === 0) {
+      this.sentVideos = [];
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableVideos.length);
+    const randomVideo = availableVideos[randomIndex];
+
+    this.sentVideos.push(randomVideo);
+
+    if (senderID !== null) {
+      message.reply({
+        body: '😥😥😥',
+        attachment: await global.utils.getStreamFromURL(randomVideo),
+      });
+
+      setTimeout(() => {
+        api.unsendMessage(loadingMessage.messageID);
+      }, 5000);
     }
   },
 };
