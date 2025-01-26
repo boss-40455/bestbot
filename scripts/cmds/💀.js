1d27a8adcd2a4b08029708b97ae9e3e894b213c0
MR-XAIKO JISAN <mshiur9080@gmail.com>
2025-01-26 19:16:03 +0600

    Create 💀.js

---
+++ scripts/cmds/💀.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+   name: "💀",
+   version: "1.0",
+   author: "Jisan",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "no prefix",
+ },
+  
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "💀") {
+ return message.reply({
+ body: "👅💀💀",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/TEBqBqj.mp4")
+ });
+ }
+ }
+}
